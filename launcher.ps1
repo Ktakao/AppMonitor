8ac5a319841072ff02052b26987521009a9aa2e4
Kazuki Takao <mikazukiuzuki@gmail.com>
2026-04-11 04:00:24 +0900

    Fix launcher

--- launcher.ps1
+++ launcher.ps1
@@ -4,10 +4,11 @@
 $BatFileName = "monitor.bat"
 # ---------------------------------------------------------
 
-# Get the path of the batch file located in the same directory as this script
-$ScriptPath = $MyInvocation.MyCommand.Path
-$Dir = Split-Path $ScriptPath -Parent
-$BatPath = Join-Path -Path $Dir -ChildPath $BatFileName
+# Get the full path of the batch file located in the same directory as this script
+$BatPath = Join-Path -Path $PSScriptRoot -ChildPath $BatFileName
+
+# Execute the batch file in a hidden window
+Start-Process -FilePath $BatPath -WindowStyle Hidden
 
 # Execute the batch file in a hidden window
 Start-Process -FilePath $BatPath -WindowStyle Hidden
